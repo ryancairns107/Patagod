487c40e994e441b5733e898a0b0105697e85475a
Patricija Barbič <pata.bba@gmail.com>
2020-06-04 21:12:17 +0200

    folder and some organization
    folder and some organization

--- Assets/Pata/PataAI.cs
+++ Assets/Pata/PataAI.cs
@@ -2,27 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PataAI : BaseAI
+public class PataAI : MonoBehaviour
 {
-    public override IEnumerator RunAI()
+    // Start is called before the first frame update
+    void Start()
     {
-        for (int i = 0; i < 10; i++)
-        {
-            yield return Ahead(100);
-            yield return FireFront(3);
-            yield return TurnLookoutLeft(50);
-            yield return TurnLeft(360);
-            yield return FireLeft(2);
-            yield return TurnLookoutRight(75);
-            yield return Back(100);
-            yield return FireRight(1);
-            yield return TurnLookoutLeft(80);
-            yield return TurnRight(90);
-        }
+        
     }
 
-    public override void OnScannedRobot(ScannedRobotEvent e)
+    // Update is called once per frame
+    void Update()
     {
-        Debug.Log("Ship detected: " + e.Name + " at distance: " + e.Distance);
+        
     }
 }
