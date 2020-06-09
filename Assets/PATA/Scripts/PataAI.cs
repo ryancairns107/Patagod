5ac20982ddc44fd451ba72ec9ed431978cb0b6d8
Patricija Barbič <pata.bba@gmail.com>
2020-06-09 15:40:52 +0200

    fixed Idle

--- Assets/PATA/Scripts/PataAI.cs
+++ Assets/PATA/Scripts/PataAI.cs
@@ -9,26 +9,29 @@ public class PataAI : MonoBehaviour
     public Transform[] approach;
     public Mangerosled manager;
     public bool enemy;
-    public bool canon;
-    public bool rock;
 
     public GameObject CannonBallPrefab = null;
     public Transform CannonFrontSpawnPoint = null;
     public Transform CannonLeftSpawnPoint = null;
     public Transform CannonRightSpawnPoint = null;
-
-    public LayerMask hmm;
-    public float maxdis = 0f;
+    public float cannonBallAmount = 20f;
+    public bool shotFired;
+    public float reload = 5f;
 
     public float check;
 
-    public Transform TRY;
+    public Vector3 IdlePosition;
+    public bool resetIdle;
+    public float loopIdle = 3f;
+    
+
 
     void Start()
     {
 
         agent = GetComponent<NavMeshAgent>();
         manager = GameObject.Find("CompetitionManager").GetComponent<Mangerosled>();
+        
 
     }
 
@@ -40,41 +43,36 @@ public class PataAI : MonoBehaviour
          approach[1] = GameObject.Find("RyanShip(Clone)").transform;
          approach[2] = GameObject.Find("JeroenShip(Clone)").transform;
 
-         if (check == 0)
-         {
-             agent.SetDestination(approach[0].position);
-             Debug.Log("FollowingOsled");
-         }
-         else if (check == 1)
-         {
-             agent.SetDestination(approach[1].position);
-             Debug.Log("FollowingRyan");
-         }
-         else if (check == 2)
-         {
-             agent.SetDestination(approach[2].position);
-             Debug.Log("FollowingJeroen");
-         }
 
-
-        StartCoroutine(__Idle());
-        StartCoroutine(__Attack());
-        StartCoroutine(__Defend());
-        StartCoroutine(__RunAway());
-
-        /*if (Physics.Raycast(transform.position, transform.forward, maxdis, hmm))
-        {
-            int rotLorR = Random.Range(1, 10);
-            if (rotLorR == 1)
+            // timer for reloading 
+            if (shotFired == true && reload > 0f)
             {
-                transform.Rotate(transform.up * 90);
+                reload -= Time.fixedDeltaTime;
+                
             }
-            if (rotLorR == 5)
+            else if (reload < 0f)
             {
-                transform.Rotate(transform.up * -90);
+                shotFired = false;
+                reload = 5f;
             }
 
-        }*/
+            // timer for Idle position
+            if (resetIdle == true && loopIdle > 0f)
+            {
+                loopIdle -= Time.fixedDeltaTime;
+                
+            }
+            else if (loopIdle < 0f)
+            {
+                resetIdle = false;
+                loopIdle = 3f;
+            }
+
+
+            StartCoroutine(__Idle());
+            StartCoroutine(__Attack());
+            StartCoroutine(__Defend());
+            StartCoroutine(__RunAway());
 
     }
 
@@ -82,38 +80,60 @@ public class PataAI : MonoBehaviour
      {
         for (var i = 0; i < approach.Length; i++)
         {
-
             if (manager.Osledhealth < manager.Ryanhealth && manager.Osledhealth < manager.Jeroenhealth)
             {
+                agent.SetDestination(approach[0].position);
+                Debug.Log("Pata Following Osled");
                 check = 0;
             }
             else if (manager.Ryanhealth < manager.Osledhealth && manager.Ryanhealth < manager.Jeroenhealth)
             {
+                agent.SetDestination(approach[1].position);
+                Debug.Log("Pata Following Ryan");
                 check = 1;
             }
             else if (manager.Jeroenhealth < manager.Ryanhealth && manager.Jeroenhealth < manager.Osledhealth)
             {
+                agent.SetDestination(approach[2].position);
+                Debug.Log("Pata Following Jeroen");
                 check = 2;
-            } else
+            } else if (resetIdle == false)
             {
                 check = 3;
-                agent.SetDestination(approach[i].position);
-                Debug.Log("IDLE BITCH");
+
+                    IdlePosition = new Vector3(Random.Range(-200.0f, 200.0f), 0, Random.Range(-200.0f, 200.0f));
+                    agent.SetDestination(IdlePosition);
+                    Debug.Log("Pata IDLE");
+
+                resetIdle = true;
             }
-
-
         }
 
         yield return new WaitForFixedUpdate();
      }
     public IEnumerator __Attack()
     {
-        if (enemy == true)
+        if (enemy == true && cannonBallAmount != 0f && shotFired == false)
         {
             GameObject newInstance = Instantiate(CannonBallPrefab, CannonFrontSpawnPoint.position, CannonFrontSpawnPoint.rotation);
             CannonBallPrefab.name = "PataCannon";
-            yield return new WaitForFixedUpdate();
+            cannonBallAmount -= 1f;
+            shotFired = true;
+
+            /*if (reload == 0)
+            {
+
+            }*/
+
+        }else if (enemy == true && cannonBallAmount == 0f)
+        {
+
         }
+        else
+        {
+
+        }
+
         yield return new WaitForFixedUpdate();
     }
     public IEnumerator __Defend()
