92377c94184ab8485800de379cf22fc5497d6e8d
TheJeronator <ensing.hj@hotmail.com>
2020-06-10 22:38:18 +0200

    jeroen AI update 4

--- Assets/Jeroen/scripts/jeroenAI.cs
+++ Assets/Jeroen/scripts/jeroenAI.cs
@@ -10,7 +10,16 @@ public class JeroenAI : MonoBehaviour
     Quaternion desiredRotation;
     public float angle;
     public float movementSpeed = 10;
+    public int currentHP;
+    public int maxHP = 100;
+    public shiphealth healthBar;
+    public int ballDamage;
+    public int deaths;
 
+    private void Start()
+    {
+        currentHP = maxHP;
+    }
     void Update()
     {
         transform.position += transform.forward * movementSpeed * Time.deltaTime;
@@ -35,12 +44,29 @@ public class JeroenAI : MonoBehaviour
             transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, 0.5f * Time.deltaTime);
         }
     }
+    private void Damaged(int damage)
+    {
+        currentHP += damage;
+        healthBar.SetHealth(currentHP);
+        if (currentHP == 0)
+        {
+            deaths += 1;
+            currentHP = 100;
+            healthBar.SetHealth(currentHP);
+        }
+
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "wall") 
         {
             desiredRotation = transform.rotation *= Quaternion.Euler(0, -180, 0);
             transform.rotation = desiredRotation;
+        }
+        if (other.gameObject.tag == "canon" || other.gameObject.tag == "Boatbody")
+        {
+            Damaged(ballDamage);
+            Debug.Log("Ship Damaged!!!");
         }
     }
     private void OnDrawGizmosSelected()
