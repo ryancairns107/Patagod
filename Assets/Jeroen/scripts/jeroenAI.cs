5754dd4cc4f0001ac9d36bc28694ebd52b87de09
TheJeronator <ensing.hj@hotmail.com>
2020-06-10 03:59:58 +0200

    update jeroen 1

--- Assets/Jeroen/scripts/jeroenAI.cs
+++ Assets/Jeroen/scripts/jeroenAI.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class jeroenAI : MonoBehaviour
 {
-    public GameObject[] SpottedShips;
-    public float sphereDist = 100;
+    public GameObject thisShip;
+    public GameObject TargetShip;
+    public float sphereSize = 100;
 
     void Update()
     {
-        
-    }
+        RaycastHit hit;
+        if (Physics.SphereCast(thisShip.transform.position, sphereSize, transform.forward, out hit, 1))
+        {
+            if(hit.transform.gameObject.tag == "Boatbody")
+            {
+                TargetShip = hit.transform.gameObject;
+            }
+            else
+            {
+                TargetShip = null;
+            }
+        }
+        if (TargetShip != null)
+        {
 
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(thisShip.transform.position, sphereSize);
+    }
 }
