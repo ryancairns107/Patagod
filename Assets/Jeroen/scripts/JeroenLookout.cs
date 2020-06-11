80f83fbedf17cd657ba81114fc4b0a8278e9d5ac
TheJeronator <ensing.hj@hotmail.com>
2020-06-11 04:07:14 +0200

    Jeroen AI update 177013

---
+++ Assets/Jeroen/scripts/JeroenLookout.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JeroenLookout : MonoBehaviour
+{
+    public JeroenAI JeroenAI;
+    public GameObject JeroenShip;
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.transform.gameObject.tag == "Boatbody" && other.transform.gameObject != JeroenShip)
+        { 
+        if (JeroenAI.Targets.Contains(other.transform.gameObject) == false)
+        {
+            JeroenAI.Targets.Add(other.transform.gameObject);
+        }
+    }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        for (int i = 0; i < JeroenAI.Targets.Count; i++)
+        {
+            if (other.transform.gameObject == JeroenAI.Targets[i])
+            {
+                JeroenAI.Targets.RemoveAt(i);
+            }
+        }
+    }
+}
